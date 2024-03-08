fde125ce4f02f19889bfccd33d9385997ac4cbb8
Ray <91828843+Alphocado@users.noreply.github.com>
2024-03-08 18:28:35 +0800

    6. View Layouts

--- framework/CodeIgniter/4/ci4app/app/Controllers/Pages.php
+++ framework/CodeIgniter/4/ci4app/app/Controllers/Pages.php
@@ -12,9 +12,7 @@ class Pages extends BaseController
     ];
 
     // bisa ganti return dengan echo
-    echo view('layout/header', $data);
-    echo view('pages/home');
-    echo view('layout/footer');
+    return view('pages/home', $data);
   }
 
   public function about()
@@ -22,8 +20,26 @@ class Pages extends BaseController
     $data = [
       'title' => 'About Me'
     ];
-    echo view('layout/header', $data);
-    echo view('pages/about');
-    echo view('layout/footer');
+    return view('pages/about', $data);
+  }
+
+  public function contact()
+  {
+    $data = [
+      'title' => 'Contact Us',
+      'alamat' => [
+        [
+          'tipe' => 'rumah',
+          'alamat' => 'Jln Sukarno',
+          'kota' => 'makassar'
+        ],
+        [
+          'tipe' => 'Kantor',
+          'alamat' => 'Jln jalanan',
+          'kota' => 'Bandung'
+        ]
+      ]
+    ];
+    return view('pages/contact', $data);
   }
 }
